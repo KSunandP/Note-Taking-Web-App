8c3d1e2645d1a3dc953f1b0d7f27bd0d6d062d79
Kyle Patel <kylepatel.kp@gmail.com>
2016-02-14 16:09:00 +0000

    Start setting up

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,23 @@
-edit
+"use strict"
+
+let gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    less = require('gulp-less'),
+    uglifycss = require('gulp-uglifycss'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    watch = require('gulp-watch');
+
+gulp.task('default', function () { console.log('Default Task') });
+
+gulp.task('styles', function(){
+    return gulp.src('./styles/**/*.less')
+    .pipe(sourcemaps.init())
+    .pipe(less())
+    .pipe(concat('styles.css'))
+    .pipe(uglifycss({"max-line-len":100}))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.watch('./styles/**/*.less', ['styles']);
--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-let server = require('express');
+"use strict"
+
+let express = require('express');
+let server = express();
+
+server.use(express.static(__dirname));
+
+server.listen(3000, function(){
+    console.log("Server started at http://localhost:3000");
+});
